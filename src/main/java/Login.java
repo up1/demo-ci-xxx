907172ba9dc8ab2e72828da8a312b58207396bfd
somkiat <somkiat.p@gmail.com>
2017-02-15 16:19:11 +0700

    First commit

---
+++ src/main/java/Login.java
@@ -0,0 +1,13 @@
+/**
+ * Created by somkiat on 2/15/2017 AD.
+ */
+public class Login {
+    public boolean checkUsername(String username) {
+        return true;
+    }
+
+
+    public boolean isExistingInDatabase(String username, String password) {
+        return true;
+    }
+}
---
+++ src/test/java/LoginSuccessTest.java
@@ -0,0 +1,18 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+
+public class LoginSuccessTest {
+
+    @Test
+    public void loginwithLDAP() {
+        Login login = new Login();
+        boolean flag1 = login.checkUsername("somkiat");
+        boolean flag2 = login.isExistingInDatabase("somkiat", "password");
+
+        assertTrue( "fsdfsdf", flag1 );
+        assertTrue( "sdsdsd", flag2 );
+    }
+
+}
