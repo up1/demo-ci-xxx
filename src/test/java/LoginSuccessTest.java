3bf37227dbe3816962a69924ead7acadf4766bfd
somkiat <somkiat.p@gmail.com>
2017-02-15 16:27:45 +0700

    Add test fail

--- src/test/java/LoginSuccessTest.java
+++ src/test/java/LoginSuccessTest.java
@@ -1,18 +1,19 @@
 import org.junit.Test;
 
+import static junit.framework.TestCase.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 
 public class LoginSuccessTest {
 
-    
+
     @Test
     public void loginwithLDAP() {
         Login login = new Login();
         boolean flag1 = login.checkUsername("somkiat");
         boolean flag2 = login.isExistingInDatabase("somkiat", "password");
 
-        assertTrue( "fsdfsdf", flag1 );
+        assertFalse( "fsdfsdf", flag1 );
         assertTrue( "sdsdsd", flag2 );
     }
 
